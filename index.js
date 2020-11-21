3e6f699c5f5a6e9675ba001dfc3c6b9bcb6d708a
Victor Marquez <vtrmq09@gmailcom>
2020-11-21 09:27:28 -0500

    Update webjs

---
+++ index.js
@@ -0,0 +1,15 @@
+async function conexion() {
+	const data = {
+    message: 'We make a research of fetch'
+  }
+	const endpoint = 'http://webjs.co/php/sqlite.php'
+	let result = await fetch(endpoint, {
+    method: 'POST',
+    body: JSON.stringify(data)
+	})
+	let resultado = await result.json()
+	//console.log(resultado)
+	document.querySelector('p').textContent = resultado[0].Mensaje
+}
+
+conexion()
